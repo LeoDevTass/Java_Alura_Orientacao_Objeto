ba8761af44f9421f2b2390b7499114d74c6aa422
fassuncao <fabiola.assuncao@foursys.com.br>
2021-04-01 15:02:52 -0300

    Incluindo classe gerente

---
+++ byteBank_herdado/src/test/java/TestGerente.java
@@ -0,0 +1,17 @@
+public class TestGerente {
+
+  public static void main(String[] args) {
+    Gerente g1 = new Gerente();
+    g1.setNome("Marco");
+    g1.setCpf("32156162");
+    g1.setSalario(5000.0);
+
+    System.out.println(g1.getNome());
+    System.out.println(g1.getCpf());
+    System.out.println(g1.getSalario());
+
+    g1.setSenha(115);
+    boolean autentica = g1.autentica(115);
+    System.out.println(autentica);
+  }
+}
