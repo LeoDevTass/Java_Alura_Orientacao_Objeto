3e900a3f83ad77ca5b43a1270aa5a2aecebde83a
fassuncao <fabiola.assuncao@foursys.com.br>
2021-04-01 12:47:21 -0300

    Revisão de construtores

---
+++ byteBank_herdado/src/test/java/testFuncionario.java
@@ -0,0 +1,13 @@
+public class testFuncionario {
+
+  public static void main(String [] args){
+
+    Funcionario nico = new Funcionario();
+    nico.setNome("Nico");
+    nico.setCpf("1654185418");
+    nico.setSalario(2600.00);
+
+    System.out.println(nico.getNome());
+    System.out.println(nico.getBonificacao());
+  }
+}
